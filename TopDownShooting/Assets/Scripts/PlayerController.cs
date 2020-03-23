68bd78e3ea1eb917e9332fb0172cd7b2cf7a530e
“Jeongwonseok” <jws4281@nate.com>
2020-03-24 00:09:23 +0900

    1. Player Controller
    - RequireComponent 사용
    - FixedUpdate() : 정기적이고 짧게 반복적으로 실행되야 하기 때문에 Update() 대신 사용
    - W A S D 키 이용해서 움직임 정의
    - Time.fixedDeltaTime : 두 FixedUpdate 메소드가 호출된 시간 간격
    - groundPlane 이용해서 카메라 시선과 플레인 시선이 교차하는 지점 바라보도록 구현

---
+++ TopDownShooting/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    Vector3 velocity;
+    Rigidbody myRigidbody;
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    // 플레이어 회전
+    // heightCorrectPoint : y는 고정하고 바라보기 >> 기울지 않도록!!
+    public void LookAt(Vector3 lookPoint)
+    {
+        Vector3 heightCorrectPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt(heightCorrectPoint);
+    }
+
+    // FixedUpdate() : 이 부분은 정기적이고 짧게 반복적으로 실행되야 하기 때문에 Update() 대신 사용
+    public void FixedUpdate()
+    {
+        // Time.fixedDeltaTime : 두 FixedUpdate 메소드가 호출된 시간 간격
+        myRigidbody.MovePosition(myRigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
