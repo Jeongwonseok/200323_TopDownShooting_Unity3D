a3f9a5bf914fc3940918f908d3e0d6139ed1462c
“Jeongwonseok” <jws4281@nate.com>
2020-03-25 02:18:53 +0900

    Enemy
    - 장애물 추가 및 Navigation 구현
    - NavMeshAgent 컴포넌트 적 오브젝트에 추가
    - 적 네비게이션 타겟 지정 및 이동
    - 코루틴으로 정의하는 이유 >> Update에 넣으면 매 프레임마다 추적하므로 게임 과부하
    - setDestination() 메서드 이용해서 타겟 추적
    - Projectile 스크립트에 CheckCollisions() , OnHitObject() 메서드 추가
    - CheckCollisions() : ray가 이동할거리, 충돌에 관한 결과 가져오기
    - OnHitObject() : 충돌 시 파괴

---
+++ TopDownShooting/Assets/Scripts/Enemy.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent (typeof (NavMeshAgent))]
+public class Enemy : MonoBehaviour
+{
+    NavMeshAgent pathFinder;
+    Transform target;
+
+    void Start()
+    {
+        pathFinder = GetComponent<NavMeshAgent>();
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+
+        StartCoroutine(UpdatePath());
+    }
+
+    void Update()
+    {
+        
+    }
+
+    // 적 네비게이션 타겟 지정 및 이동
+    // 코루틴으로 정의하는 이유 >> Update에 넣으면 매 프레임마다 추적하므로 게임 과부하
+    IEnumerator UpdatePath()
+    {
+        float refreshRate = 0.25f;
+
+        while(target != null)
+        {
+            Vector3 targetPosition = new Vector3(target.position.x, 0, target.position.z);
+            pathFinder.SetDestination(targetPosition);
+            yield return new WaitForSeconds(refreshRate);
+        }
+    }
+}
--- TopDownShooting/Assets/Scripts/Projectile.cs
+++ TopDownShooting/Assets/Scripts/Projectile.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public LayerMask collisionMask;
+
     float speed = 10f;
 
     public void SetSpeed(float newSpeed)
@@ -13,7 +15,33 @@ public class Projectile : MonoBehaviour
 
     void Update()
     {
+        float moveDistance = speed * Time.deltaTime;
+
+        CheckCollisions(moveDistance);
+
         // 총알 Shoot
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * moveDistance);
+    }
+
+    // ray가 이동할거리, 충돌에 관한 결과 가져오기
+    void CheckCollisions(float moveDistance)
+    {
+        // 발사체 앞 방향으로 ray 발사
+        Ray ray = new Ray(transform.position, transform.forward);
+        // 발사체와 충돌한 오브젝트에 대한 반환 정보
+        RaycastHit hit;
+
+        // QueryTriggerInteraction.Collide >> 트리거 콜라이더들과 충돌할지 안할지 정해준다.
+        if (Physics.Raycast(ray, out hit, moveDistance, collisionMask, QueryTriggerInteraction.Collide))
+        {
+            OnHitObject(hit);
+        }
+    }
+
+    // 충돌 시 파괴
+    void OnHitObject(RaycastHit hit)
+    {
+        print(hit.collider.gameObject.name);
+        GameObject.Destroy(gameObject);
     }
 }
