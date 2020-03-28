e219665c1d0ec2ee6c46ee4bb9473e98181ae952
“Jeongwonseok” <jws4281@nate.com>
2020-03-29 01:01:31 +0900

    Tile Map
    - [CustomEditor (typeof (MapGenetator))] 키워드 사용
    - 맵 custom 하기 >> 실시간으로 Edit
    - DestroyImmediate() : 에디터에서 호출하기 위해 이 메서드 사용해서 오브젝트 파괴

---
+++ TopDownShooting/Assets/Editor/MapEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+// 맵 custom 하는 방법
+[CustomEditor (typeof (MapGenetator))]
+public class MapEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        // CustomEditor 키워드로 이 에디터 스크립트가 다룰것이라고 선언한 오브젝트가 자동으로 target 변수에 설정된다.
+        MapGenetator map = target as MapGenetator;
+
+        map.GenerateMap();
+    }
+}
---
+++ TopDownShooting/Assets/Scripts/MapGenetator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenetator : MonoBehaviour
+{
+    public Transform tilePrefab;
+    public Vector2 mapSize;
+
+    [Range(0,1)]
+    public float outlinePercent; // 타일 구분
+
+    void Start()
+    {
+        GenerateMap();
+    }
+
+    // 맵 그리기
+    public void GenerateMap()
+    {
+        string holderName = "Generated Map";
+
+        // 에디터에서 호출하려면 DestroyImmediate() 사용
+        if(transform.FindChild(holderName))
+        {
+            DestroyImmediate(transform.FindChild(holderName).gameObject);
+        }
+
+        Transform mapHolder = new GameObject(holderName).transform;
+        mapHolder.parent = transform;
+
+        for(int x=0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                // 맵의 가로 길이의 절반 만큼 왼쪽으로 이동한 점에서부터 타일 생성 시작
+                Vector3 tilePosition = new Vector3(-mapSize.x/2 + 0.5f + x, 0, -mapSize.y/2 + 0.5f + y);
+                Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90)) as Transform;
+                newTile.localScale = Vector3.one * (1 - outlinePercent);
+                newTile.parent = mapHolder;
+            }
+        }
+    }
+}
